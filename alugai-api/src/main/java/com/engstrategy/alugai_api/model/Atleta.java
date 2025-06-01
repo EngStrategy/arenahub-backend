49e23f7a59c4f5da651fee1c5d828758603be212
Renan Soares <renanalencar313@gmail.com>
2025-06-01 16:06:14 -0300

    Adição de testes unitários, remoção de anotações lombok em entidades, adição de perfil test com H2

--- alugai-api/src/main/java/com/engstrategy/alugai_api/model/Atleta.java
+++ alugai-api/src/main/java/com/engstrategy/alugai_api/model/Atleta.java
@@ -2,6 +2,17 @@ package com.engstrategy.alugai_api.model;
 
 import jakarta.persistence.Entity;
 
+import java.util.UUID;
+
 @Entity
 public class Atleta extends Usuario {
+
+    // Construtor vazio (necessário para o JPA)
+    public Atleta() {
+    }
+
+    // Construtor com os campos herdados de Usuario, se necessário
+    public Atleta(UUID id, String nome, String email, String telefone, String senha) {
+        super(id, nome, email, telefone, senha);
+    }
 }
--- alugai-api/src/main/java/com/engstrategy/alugai_api/model/Usuario.java
+++ alugai-api/src/main/java/com/engstrategy/alugai_api/model/Usuario.java
@@ -5,14 +5,7 @@ import lombok.*;
 
 import java.util.UUID;
 
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-@Setter
-@Getter
 @MappedSuperclass
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Usuario {
 
     @Id
@@ -31,6 +24,58 @@ public abstract class Usuario {
     @Column(nullable = false)
     private String senha;
 
+    // Construtor vazio
+    public Usuario() {
+    }
 
+    // Construtor com todos os campos
+    public Usuario(UUID id, String nome, String email, String telefone, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.senha = senha;
+    }
+
+    // Getters e setters
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 
 }
