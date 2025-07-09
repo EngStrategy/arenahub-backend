b3cdc6c807912a901443a10ba8a91a5fbe246e2d
Rian Lima <rlimamendes@proton.me>
2025-07-09 19:39:56 -0300

    Intervalos de reserva retornados ao recuperar quadras

--- alugai-api/src/main/java/com/engstrategy/alugai_api/dto/quadra/QuadraResponseDTO.java
+++ alugai-api/src/main/java/com/engstrategy/alugai_api/dto/quadra/QuadraResponseDTO.java
@@ -51,4 +51,7 @@ public class QuadraResponseDTO {
 
     @Schema(description = "Nome da arena associada", example = "Arena Sports Center")
     private String nomeArena;
+
+    @Schema(description = "Horários de reserva da quadra")
+    private List<HorarioFuncionamentoResponseDTO> horariosFuncionamento;
 }
