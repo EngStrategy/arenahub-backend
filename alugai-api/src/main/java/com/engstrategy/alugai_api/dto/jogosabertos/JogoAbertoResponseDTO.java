e1deca91bd44794545bd1b77f73476ed8031628c
Renan Soares <renanalencar313@gmail.com>
2025-07-12 18:43:52 -0300

    validacao para solicitar/cancelar entrada em jogo

--- alugai-api/src/main/java/com/engstrategy/alugai_api/dto/jogosabertos/JogoAbertoResponseDTO.java
+++ alugai-api/src/main/java/com/engstrategy/alugai_api/dto/jogosabertos/JogoAbertoResponseDTO.java
@@ -1,6 +1,8 @@
 package com.engstrategy.alugai_api.dto.jogosabertos;
 
 import com.engstrategy.alugai_api.model.enums.TipoEsporte;
+import com.engstrategy.alugai_api.util.LocalTimeSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Builder;
 import lombok.Data;
 
@@ -13,8 +15,13 @@ import java.time.LocalTime;
 public class JogoAbertoResponseDTO {
     private Long agendamentoId;
     private LocalDate data;
+
+    @JsonSerialize(using = LocalTimeSerializer.class)
     private LocalTime horarioInicio;
+
+    @JsonSerialize(using = LocalTimeSerializer.class)
     private LocalTime horarioFim;
+
     private Integer vagasDisponiveis;
     private TipoEsporte esporte;
     private BigDecimal valorPorPessoa;
